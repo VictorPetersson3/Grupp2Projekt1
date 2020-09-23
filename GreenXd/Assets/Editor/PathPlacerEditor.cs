8ef7cb52032205594ee0dd10b5a25d575a64bcce
leovonahntga <71003064+leovonahntga@users.noreply.github.com>
2020-09-23 17:32:48 +0200

    add unity project

---
+++ GreenXd/Assets/Editor/PathPlacerEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PathPlacer))]
+public class PathPlacerEditor : Editor
+{
+    PathPlacer pathPlacer;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUI.BeginChangeCheck();
+
+        if (GUILayout.Button("Create spheres"))
+        {
+            Undo.RecordObject(pathPlacer, "Create spheres");
+            pathPlacer.CreateSpheres();
+        }
+    }
+
+    void OnEnable()
+    {
+        pathPlacer = (PathPlacer)target;
+    }
+}
---
+++ GreenXd/Assets/Scripts/SplineManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineManager : MonoBehaviour
+{
+    private PathCreator[] pathCreators;
+    
+    void Start()
+    {
+        pathCreators = GetComponentsInChildren<PathCreator>();
+    }
+
+    public Vector2 GetClosestPoint(Vector2 aPosition, ref int aPointsIndex, ref Vector2[] aPoints)
+    {
+        Vector2 closestPoint = Vector2.zero;
+
+        for (int i = 0; i < pathCreators.Length; i++)
+        {
+            Vector2 closestPointInIteration = pathCreators[i].GetClosestPoint(aPosition);
+
+            if (closestPoint == Vector2.zero)
+            {
+                closestPoint = closestPointInIteration;
+            }
+            else if (Vector2.Distance(aPosition, closestPointInIteration) < Vector2.Distance(aPosition, closestPoint))
+            {
+                Debug.Log(this + " found new closest point.");
+                closestPoint = closestPointInIteration;
+                aPointsIndex = i;
+                aPoints = pathCreators[i].path.CalculateEvenlySpacedPoints();
+            }
+        }
+
+        return closestPoint;
+    }
+}
