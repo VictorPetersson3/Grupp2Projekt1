4f73c708e6bc9d3dfe74eb912d7ae106b9a0f433
LEARNET\morgan.waernwickstr <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-09-29 16:51:56 +0200

    Added Screenshake script, but not implemented.
    Moved Camera and CameraShake scripts into seperate folder only for camera functionality.

---
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private float myShakeDuration;
+    private float myShakeMagnitude;
+    private float myDampingSpeed = 1f;
+    private Vector3 myOriginalPosition;
+
+    private void OnEnable()
+    {
+        myOriginalPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (myShakeDuration > 0)
+        {
+            transform.localPosition = myOriginalPosition + Random.insideUnitSphere * myShakeMagnitude;
+            myShakeDuration -= Time.deltaTime * myDampingSpeed;
+        }
+        else
+        {
+            myShakeDuration = 0f;
+            transform.localPosition = myOriginalPosition;
+        }
+    }
+
+    public void TriggerShake(float aShakeDuration, float aShakeMagnitude)
+    {
+        myShakeDuration = aShakeDuration;
+        myShakeMagnitude = aShakeMagnitude;
+    }
+}
