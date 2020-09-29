b1ed0fe0244232a99019e3be7807f00fd23aa3b6
LEARNET\morgan.waernwickstr <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-09-29 16:11:19 +0200

    Added death on collision with rocks

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private Vector3 myOriginalPosition;
+
+    void Start()
+    {
+        myOriginalPosition = transform.position;
+    }
+
+    public void Die()
+    {
+        transform.position = myOriginalPosition;
+    }
+}
