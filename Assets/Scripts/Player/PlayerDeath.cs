46176972371ee2cae05a23ffa05256dd2673c6c7
LEARNET\morgan.waernwickstr <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-09-29 16:20:13 +0200

    Added reset to rotation aswell on death

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -3,14 +3,17 @@
 public class PlayerDeath : MonoBehaviour
 {
     private Vector3 myOriginalPosition;
+    private Quaternion myOriginalRotation;
 
     void Start()
     {
         myOriginalPosition = transform.position;
+        myOriginalRotation = transform.rotation;
     }
 
     public void Die()
     {
         transform.position = myOriginalPosition;
+        transform.rotation = myOriginalRotation;
     }
 }
