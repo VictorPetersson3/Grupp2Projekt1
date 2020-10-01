89db2ee38eb085d3c14ae9acae4658c337c5b53f
LEARNET\david.falck <david.falck@studerande.thegameassembly.se>
2020-10-01 15:33:14 +0200

    Main menu som leder till spelets första bana

---
+++ Assets/Scripts/SceneSelect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using System.Collections; ---> Vet inte om den här behövs tbh
+
+public class SceneSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Space))
+        {
+            StartingTheGame();
+        }
+        if (Input.GetKey("escape"))
+        {
+            QuittingTheGame();
+        }
+
+    }
+
+    public void StartingTheGame()
+    {
+
+        Debug.Log("Laddar \"CollisionTestScene\"");
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    public void QuittingTheGame()
+    {
+        Debug.Log("Quitting the fucking game");
+        Application.Quit();
+    }
+
+}
+
+
+
