e87597e80db2c024bc1c92934ad1999888f70f6c
LEARNET\victor.petersson3 <victor.petersson@studerande.thegameassembly.se>
2020-10-01 09:36:58 +0200

    Mesh generering för splinesen, första draften

---
+++ Assets/Editor/MeshEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshCreator))]
+public class MeshEditor : Editor
+{
+    private MeshCreator creator;
+    bool updateMesh = false;
+
+
+    private void OnSceneGUI()
+    {
+        if (creator.previewGeneratedMesh && Event.current.type == EventType.Repaint)
+        {
+            creator.UpdateMesh();
+        }
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUI.BeginChangeCheck();
+        
+        GUILayout.Label(" ");
+        //updateMesh = GUILayout.Toggle(updateMesh, "Update mesh while editing.");
+        //{
+        //    creator.previewGeneratedMesh = true;
+        //}
+        if (GUILayout.Button("Update 3D mesh"))
+        {
+            Undo.RecordObject(creator, "Update 3D mesh");
+            creator.UpdateMesh();
+        }
+        GUILayout.Label(" ");
+        GUILayout.Label("Options to manipulate the mesh on the spline.");
+        GUILayout.Label(" ");
+        if (GUILayout.Button("Generate 3D Mesh"))
+        {
+            Undo.RecordObject(creator, "Generate 3D Mesh");
+            creator.CreateMesh();
+        }
+    }
+
+    private void OnEnable()
+    {
+        creator = (MeshCreator)target;
+    }
+}
