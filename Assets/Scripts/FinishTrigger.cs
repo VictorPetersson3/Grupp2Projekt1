f2ce49217744c9cfcd90759f110f4a73d4e7460d
rutqvistgustav <gustav.rutqvist@studerande.thegameassembly.se>
2020-10-06 14:22:25 +0200

    renamed to finishtrigger

---
+++ Assets/Scripts/FinishTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider aCollider)
+    {
+        Debug.Log("Finished Level!");
+    }
+}
