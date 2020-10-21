5678a3299304c14d14c8c83fb1e9dfa7f0375fba
bjornelfTGA <bjorn.elf@studerande.thegameassembly.se>
2020-10-21 15:16:17 +0200

    Commit leker med ljud

--- Assets/Scripts/Sound/MusicManagerScript.cs
+++ Assets/Scripts/Sound/MusicManagerScript.cs
@@ -1,19 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*******************************************
-* Test-funktioner just nu för kontinuerlig bakgrundsmusik.
-* Just nu endast en fil (menymusik).
-* 
-* PlayMenuMusic();
-* StopMenuMusic();
-* 
-* Musiken måste stängas av i slutet av varje bana/åk/död.
-* Tanken är att utveckla vidare till separat musik 
-* för Meny samt respektive bana. 
-* 
-* 
-*******************************************/
+
 
 public class MusicManagerScript : MonoBehaviour
 {
@@ -23,56 +11,24 @@ public class MusicManagerScript : MonoBehaviour
     [SerializeField]
     private AudioClip myMenuMusicClip;
 
-    //[SerializeField]
-    //private AudioClip myScene01MusicClip;
+    [SerializeField]
+    private AudioClip myScene01MusicClip;
     //[SerializeField]
     //private AudioClip myScene02MusicClip;
     //[SerializeField]
     //private AudioClip myScene03MusicClip;
-    //[SerializeField]
-    //private AudioClip myScene04MusicClip;
 
 
 
-    //[SerializeField]
-    //private AudioClip myPauseResumeClip;
-    //[SerializeField]
-    //private AudioClip myQuitGameClip;
-
-    //[SerializeField]
-    //private AudioClip myBoardHumClip;
-    //[SerializeField]
-    //private AudioClip myBoardJumpClip;
-    //[SerializeField]
-    //private AudioClip myBoardLandClip;
-
-    [SerializeField]
-    private AudioClip myCoinCollectClip;
-    //[SerializeField]
-    //private AudioClip myRailRideClip;
-    //[SerializeField]
-    //private AudioClip myPowerUpClip;
-    //[SerializeField]
-    //private AudioClip myPowerDownClip;
-    //[SerializeField]
-    //private AudioClip myCollisionClip;
-    //[SerializeField]
-    //private AudioClip myDeathCrashClip;
 
     //[SerializeField]
     private AudioSource myMusicSource;
-    ////[SerializeField]
-    //private AudioSource mySoundFXSource;
-    ////[SerializeField]
-    //private AudioSource myBackgroundNoiseSource;
-    ////[SerializeField]
-    //private AudioSource myBoardSoundSource;
 
 
     //[SerializeField]
     private float myMusicVolume = 1f; 
-    [SerializeField]
-    private float myFadeTime = 0.5f;  //sek, ca
+    //[SerializeField]
+    private float myFadeTime = 0.4f;  //sek, ca
 
     private bool myFadeUp = false;
     private bool myFadeDown = false;
@@ -102,48 +58,64 @@ public class MusicManagerScript : MonoBehaviour
     void Update()
     {
         // bara för test!
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))  //  Man behöver ingen separat playfunktion nedan funkar. Bra om mna kan fada ut dock!  2020 10 20 BE
         {
-            PlayMenuMusic();
+            PlayMusic();
         }
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            StopMenuMusic();
+            StopMusic();
         }
 
-        if (myFadeUp)
+        // bara för test!
+        if (Input.GetKeyDown(KeyCode.A))        //FUNKAR!! 2020 10 20 BE
         {
-            myMusicVolume += Time.deltaTime / myFadeTime;
-            if (myMusicVolume > 1f)
-            {
-                myMusicVolume = 1;
-                myFadeUp = false;
-            }
-            myMusicSource.volume = myMusicVolume;
+            StopMusic();
+            myMusicSource.clip = myScene01MusicClip;
+            PlayMusic();
+        }
+        if (Input.GetKeyDown(KeyCode.S))        //FUNKAR!! 2020 10 20 BE
+        {
+            StopMusic();
+            myMusicSource.clip = myMenuMusicClip;
+            PlayMusic();
         }
 
-        if (myFadeDown)
-        {
-            myMusicVolume -= Time.deltaTime / myFadeTime;
-            if (myMusicVolume <= 0.01f)
-            {
-                myMusicVolume = 0;
-                myMusicSource.Stop();
-                myFadeDown = false;
-            }
-            myMusicSource.volume = myMusicVolume;
-        }
+
+        //if (myFadeUp)
+        //{
+        //    myMusicVolume += Time.deltaTime / myFadeTime;
+        //    if (myMusicVolume > 1f)
+        //    {
+        //        myMusicVolume = 1;
+        //        myFadeUp = false;
+        //    }
+        //    myMusicSource.volume = myMusicVolume;
+        //}
+
+        //if (myFadeDown)
+        //{
+        //    myMusicVolume -= Time.deltaTime / myFadeTime;
+        //    if (myMusicVolume <= 0.01f)
+        //    {
+        //        myMusicVolume = 0;
+        //        myMusicSource.Stop();
+        //        myFadeDown = false;
+        //    }
+        //    myMusicSource.volume = myMusicVolume;
+        //}
     }
 
 
-    public void PlayMenuMusic ()
+    public void PlayMusic ()
     {
-        myMusicSource.Play();
-        myFadeUp = true;
+        myMusicSource.Play();  //!
+        //myFadeUp = true; //!
     }
-    public void StopMenuMusic ()
+    public void StopMusic ()
     {
-        myFadeDown = true;
+        myMusicSource.Stop();  //EXTRA
+        //myFadeDown = true;  //!
     }
 
     void PlayCoinSound()
