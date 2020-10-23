8adb5a3d8d0c1231a645fc1dcceb1df7a46ea048
LEARNET\david.falck <david.falck@studerande.thegameassembly.se>
2020-10-23 18:13:54 +0200

    Det finns nu en tutorial till bana 1 som lär spelaren hur man hoppar och voltar.

---
+++ Assets/Scripts/UI/SecondTutorialScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondTutorialScript : MonoBehaviour
+{
+    private GameObject myPlayer = null;
+    private bool myHaveEntered = false;
+    [SerializeField] private GameObject mySecondTutorialText = null;
+
+    private void Start()
+    {
+        myPlayer = GameObject.FindGameObjectWithTag("Player");
+        if (myPlayer == null)
+        {
+            Debug.LogError("myPlayer: " + myPlayer);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && myHaveEntered == true)
+        {
+            Time.timeScale = 1;
+            Destroy(this.gameObject);
+            Debug.Log("du förstörde tutroial box, you monster");
+        }
+    }
+    private void OnCollisionEnter(Collision aCollision)
+    {
+        if (aCollision.gameObject.CompareTag("Player"))
+        {
+            myHaveEntered = true;
+            Time.timeScale = 0.1f;
+            mySecondTutorialText.SetActive(true);//skapa något, point to world? Fråga Sparky
+            Debug.Log("du är i tutorialbox");
+        }
+    }
+}
