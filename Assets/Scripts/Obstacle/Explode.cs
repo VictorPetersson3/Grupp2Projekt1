d7a9557ec1fb7aea60e3145a96b888e205b803a0
morganwaernwickstrom <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-10-25 18:05:04 +0100

    Added shield power up
    * Explosion for rock not yet working

--- Assets/Scripts/Obstacle/Explode.cs
+++ Assets/Scripts/Obstacle/Explode.cs
@@ -1,7 +1,13 @@
-﻿using UnityEngine;
+﻿using Boo.Lang;
+using UnityEngine;
 
 public class Explode : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject myRockParticle = null;
+    // Currently using same container as for sandparticles
+    private GameObject mySandParticlesContainer = null;
+    private List<GameObject> myRockParticles = new List<GameObject>();
     private Player myPlayer = null;
 
     void Start()
@@ -11,20 +17,49 @@ public class Explode : MonoBehaviour
         {
             Debug.LogError("myPlayer: " + myPlayer);
         }
+        mySandParticlesContainer = GameObject.FindGameObjectWithTag("SandParticles");
+        if (mySandParticlesContainer == null)
+        {
+            Debug.LogError("mySandParticleContainer: " + mySandParticlesContainer);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = myRockParticles.Count - 1; i >= 0; i--)
+        {
+            // Remove
+            GameObject currentObject = myRockParticles[i];
+            SandParticle currentParticle = currentObject.GetComponent<SandParticle>();
+            if (currentParticle.myIsDead)
+            {
+                myRockParticles.RemoveAt(i);
+                Destroy(currentObject);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision aCollision)
     {
         if (myPlayer.GetInvincible())
         {
-            Debug.Log("Should explode");
-            //CreateExplosion();
+            // TODO: Fix Explosion
+            Destroy(gameObject);
+            //CreateExplosion(25);
         }
 
     }
 
-    private void CreateExplosion()
+    private void CreateExplosion(int aTimes)
     {
+        for (int i = 0; i < aTimes; i++)
+        {
+            float newX = Random.Range(transform.position.x - 5, transform.position.x + 5);
+            float newY = Random.Range(transform.position.y - 5, transform.position.y + 5);
+            Vector3 newPosition = new Vector3(newX, newY, 0);
 
+            GameObject particle = Instantiate(myRockParticle, newPosition, Quaternion.identity, mySandParticlesContainer.transform);
+            myRockParticles.Add(particle);
+        }
     }
 }
--- Assets/Scripts/Particles/RockParticle.cs
+++ Assets/Scripts/Particles/RockParticle.cs
@@ -1,18 +1,96 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RockParticle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Lifetime
+    private float myMinLifeTime = 0.3f;
+    private float myMaxLifeTime = 0.6f;
+    private float myTotalLifeTime = 0f;
+    private float myLifeTime = 0f;
+
+    // Physics
+    private float myGravity = 4f;
+    private float myMaxXForce = 7;
+    private float myMinXForce = 2;
+    private float myMaxYForce = 7f;
+    private float myMinYForce = 1f;
+
+    // Color and size
+    private float myDecreseOpacity = 0.02f;
+    private Vector3 myScaleChange = new Vector3(0.5f, 0.5f, 0);
+    private Material myMaterial;
+    private Color myOriginalColor;
+    private Color myNewColor;
+
+    [HideInInspector]
+    public bool myIsDead = false;
+
+    private void Start()
     {
-        
+        // Set life
+        SetLifeTime();
+
+        // Render
+        myMaterial = GetComponent<MeshRenderer>().material;
+        myOriginalColor = myMaterial.color;
+        myNewColor = myOriginalColor;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CheckIfDead();
+        IncreaseSize();
+        IncreaseTransparency();
+
+        if (myLifeTime >= myTotalLifeTime / Random.Range(2, 4))
+        {
+            ApplyGravity();
+            return;
+        }
+        ApplyForce();
+    }
+
+    private void CheckIfDead()
+    {
+        myLifeTime += Time.deltaTime;
+        if (myLifeTime > myTotalLifeTime)
+        {
+            myIsDead = true;
+        }
+    }
+
+    private void SetLifeTime()
+    {
+        myTotalLifeTime = Random.Range(myMinLifeTime, myMaxLifeTime);
+    }
+
+    private void IncreaseTransparency()
+    {
+        if (myNewColor.a > 0.1f)
+        {
+            myNewColor.a -= myDecreseOpacity;
+            myMaterial.SetColor("_Color", myNewColor);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (myLifeTime >= myTotalLifeTime / 3)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - (myGravity * Time.deltaTime), transform.position.z);
+        }
+    }
+
+    private void ApplyForce()
+    {
+        float newYForce = Random.Range(myMinYForce, myMaxYForce);
+        float newXForce = Random.Range(myMinXForce, myMaxXForce);
+
+        transform.position = new Vector3(transform.position.x + (newXForce * Time.deltaTime), transform.position.y + (newYForce * Time.deltaTime), transform.position.z);
+    }
+
+    private void IncreaseSize()
+    {
+        transform.localScale += myScaleChange * Time.deltaTime;
     }
 }
