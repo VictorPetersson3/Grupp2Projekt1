c03f8d395b2d84cb7447eac83b9e0d473b85e2b6
morganwaernwickstrom <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-10-25 21:13:22 +0100

    Added particle system to rock explosion

--- Assets/Scripts/Obstacle/Explode.cs
+++ Assets/Scripts/Obstacle/Explode.cs
@@ -1,41 +1,21 @@
-﻿using Boo.Lang;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Explode : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject myRockParticle = null;
-    // Currently using same container as for sandparticles
-    private GameObject mySandParticlesContainer = null;
-    private List<GameObject> myRockParticles = new List<GameObject>();
     private Player myPlayer = null;
+    private GameObject myParent = null;
+    private GameObject myGraphicsContainer = null;
+    private ParticleSystem myExplosion = null;
 
     void Start()
     {
         myPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        myParent = transform.parent.gameObject;
+        myGraphicsContainer = GameObject.Find("capsulCrashed");
+        myExplosion = gameObject.GetComponentInParent(typeof(ParticleSystem)) as ParticleSystem;
         if (myPlayer == null)
         {
             Debug.LogError("myPlayer: " + myPlayer);
-        }
-        mySandParticlesContainer = GameObject.FindGameObjectWithTag("SandParticles");
-        if (mySandParticlesContainer == null)
-        {
-            Debug.LogError("mySandParticleContainer: " + mySandParticlesContainer);
-        }
-    }
-
-    private void Update()
-    {
-        for (int i = myRockParticles.Count - 1; i >= 0; i--)
-        {
-            // Remove
-            GameObject currentObject = myRockParticles[i];
-            SandParticle currentParticle = currentObject.GetComponent<SandParticle>();
-            if (currentParticle.myIsDead)
-            {
-                myRockParticles.RemoveAt(i);
-                Destroy(currentObject);
-            }
         }
     }
 
@@ -44,22 +24,10 @@ public class Explode : MonoBehaviour
         if (myPlayer.GetInvincible())
         {
             // TODO: Fix Explosion
-            Destroy(gameObject);
-            //CreateExplosion(25);
+            myExplosion.Play();
+            myGraphicsContainer.SetActive(false);
+            Destroy(myParent, myExplosion.main.duration);
         }
 
-    }
-
-    private void CreateExplosion(int aTimes)
-    {
-        for (int i = 0; i < aTimes; i++)
-        {
-            float newX = Random.Range(transform.position.x - 5, transform.position.x + 5);
-            float newY = Random.Range(transform.position.y - 5, transform.position.y + 5);
-            Vector3 newPosition = new Vector3(newX, newY, 0);
-
-            GameObject particle = Instantiate(myRockParticle, newPosition, Quaternion.identity, mySandParticlesContainer.transform);
-            myRockParticles.Add(particle);
-        }
     }
 }
