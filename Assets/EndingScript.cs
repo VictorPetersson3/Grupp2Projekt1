1cce90e2658ae5c9b93445e43b9e1bc4822bc3ff
LEARNET\david.falck <david.falck@studerande.thegameassembly.se>
2020-10-28 17:24:05 +0100

    Ending finalized (inte vad vi vill ha men fuckitttttt)

--- Assets/EndingScript.cs
+++ Assets/EndingScript.cs
@@ -7,11 +7,11 @@ using UnityEngine.SceneManagement;
 public class EndingScript : MonoBehaviour
 {
     [SerializeField] private GameObject myEndingCanvas = null;
-    [SerializeField] private GameObject myCogsCollected = null;
-    [SerializeField] private GameObject myLockedScene = null;
-    [SerializeField] private GameObject myFirstScene = null;
-    [SerializeField] private GameObject mySecondScene = null;
-    [SerializeField] private GameObject myThirdScene = null;
+    //[SerializeField] private GameObject myCogsCollected = null;
+    //[SerializeField] private GameObject myLockedScene = null;
+    //[SerializeField] private GameObject myFirstScene = null;
+    //[SerializeField] private GameObject mySecondScene = null;
+    //[SerializeField] private GameObject myThirdScene = null;
 
     //snakca med gustav om antal cogs
 
@@ -23,26 +23,26 @@ public class EndingScript : MonoBehaviour
         {
             Debug.LogError("myBG is fuckywucky");
         }
-        if (myCogsCollected == null)
-        {
-            Debug.LogError("myCogsCollected is fuckywucky");
-        }
-        if (myLockedScene == null)
-        {
-            Debug.LogError("myLockedScreen is fuckywucky");
-        }
-        if (myFirstScene == null)
-        {
-            Debug.LogError("myFirstScene is fuckywucky");
-        }
-        if (mySecondScene == null)
-        {
-            Debug.LogError("mySecondScene is fuckywucky");
-        }
-        if (myThirdScene == null)
-        {
-            Debug.LogError("myThirdScene is fuckywucky");
-        }
+        //if (myCogsCollected == null)
+        //{
+        //    Debug.LogError("myCogsCollected is fuckywucky");
+        //}
+        //if (myLockedScene == null)
+        //{
+        //    Debug.LogError("myLockedScreen is fuckywucky");
+        //}
+        //if (myFirstScene == null)
+        //{
+        //    Debug.LogError("myFirstScene is fuckywucky");
+        //}
+        //if (mySecondScene == null)
+        //{
+        //    Debug.LogError("mySecondScene is fuckywucky");
+        //}
+        //if (myThirdScene == null)
+        //{
+        //    Debug.LogError("myThirdScene is fuckywucky");
+        //}
     }
 
     void Update()
@@ -58,23 +58,23 @@ public class EndingScript : MonoBehaviour
     }*/
     private void OnTriggerEnter(Collider anEndingCollider)
     {
-        Debug.Log("We out here fam");
-        GameManager gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-        gameManager.myLevelComplete = true;
+        //Debug.Log("We out here fam");
+        //GameManager gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        //gameManager.myLevelComplete = true;
         Player player = anEndingCollider.transform.GetComponentInParent<Player>();
         player.LevelComplete();
         myEndingCanvas.SetActive(true);
-        int cogsToDisplay = gameManager.TotalCogsCollected();
-        myCogsCollected.GetComponent<Text>().text = cogsToDisplay + "/299"; //dubbelkolla on build
+        //int cogsToDisplay = gameManager.TotalCogsCollected();
+        //myCogsCollected.GetComponent<Text>().text = cogsToDisplay + "/299"; //dubbelkolla on build
 
-        if (cogsToDisplay >= 2)
-        {
-            mySecondScene.SetActive(true);
-            if (cogsToDisplay >= 100)
-            {
-                myThirdScene.SetActive(true);
-            }
-        }
+        //if (cogsToDisplay >= 2)
+        //{
+        //    mySecondScene.SetActive(true);
+        //    if (cogsToDisplay >= 100)
+        //    {
+        //        myThirdScene.SetActive(true);
+        //    }
+        //}
     }
 
     public void BackToMenu()
